0c4baab9d828ff41f34b7d523ff179e0d7b3d14e
akhilennem <105033535+akhilennem@users.noreply.github.com>
2022-06-25 16:10:01 +0530

    Update SpringBootLoginExampleApplication.java

--- src/main/java/com/attendance/login/SpringBootLoginExampleApplication.java
+++ src/main/java/com/attendance/login/SpringBootLoginExampleApplication.java
@@ -2,12 +2,25 @@ package com.attendance.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 public class SpringBootLoginExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLoginExampleApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://ptf-app.herokuapp.com/");
+			}
+		};
 	}
 
 }
